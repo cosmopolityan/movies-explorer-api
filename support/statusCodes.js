b44bc571e05bc2fad2ce8bd6f49e37962b5c5c76
Mark <cosmopolityan@yandex.ru>
2022-09-08 03:22:15 +0300

    add: support folders: errors + support

---
+++ support/statusCodes.js
@@ -0,0 +1,12 @@
+const StatusCodes = {
+  created: 201,
+  badRequest: 400,
+  unauthorized: 401,
+  forbidden: 403,
+  notFound: 404,
+  conflict: 409,
+  internal: 500,
+  mongo: 11000,
+};
+
+module.exports = { StatusCodes };

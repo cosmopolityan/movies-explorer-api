38867c34b819421946f7b58edbf4708e4ec0194c
Mark <cosmopolityan@yandex.ru>
2022-09-11 21:51:43 +0300

    fix: resolve trouble with mongoose.connect

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const helmet = require('helmet');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
@@ -7,11 +8,17 @@ const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const rateLimiter = require('./middlewares/rateLimiter');
 const errorHandler = require('./middlewares/errorHandler');
+const { MONGO } = require('./support/constants');
 
 const { PORT = 3000, HOST = 'localhost' } = process.env;
 
 const app = express();
-
+//
+mongoose.connect(MONGO, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+//
 app.use(requestLogger);
 
 app.use(
@@ -21,7 +28,7 @@ app.use(
     credentials: true,
     origin: [
       'https://movies.cosmopolityan.students.nomoredomains.sbs',
-      'https://localhost:3000', //
+      'https://localhost:3000',
     ],
   }),
 );
